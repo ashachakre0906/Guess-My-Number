470828b9fc51be129c78cd73fa70f0db7ece0932
ashachakre0906 <chourpagar.asha@gmail.com>
2023-03-03 14:31:43 -0800

    DOM manipulation to access HTML ELEMENTS and manipulating them

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,4 @@
 'use strict';
+console.log(document.querySelector('.message'));
+//DOM is documnet object model basically structured representation of the html element allows access JS to access HTML elemnets and syles to manipulate them.
+document.querySelector('.message').textContent = "🎉 Correct Number";
