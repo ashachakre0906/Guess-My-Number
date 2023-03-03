8fbffdac2215f73580bc91d7529eb1137a12be46
ashachakre0906 <chourpagar.asha@gmail.com>
2023-03-03 14:58:00 -0800

    using DOM manipulation we grabbed the guessed value and logged it in console

--- starter/script.js
+++ starter/script.js
@@ -2,3 +2,11 @@
 console.log(document.querySelector('.message'));
 //DOM is documnet object model basically structured representation of the html element allows access JS to access HTML elemnets and syles to manipulate them.
 document.querySelector('.message').textContent = "🎉 Correct Number";
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+const guessValue = document.querySelector('.guess')
+const buttonEl = document.querySelector('.check');
+buttonEl.addEventListener('click',function(){
+    console.log('button clicked');
+console.log(guessValue.value);
+});
