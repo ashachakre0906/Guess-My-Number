a9fc652e20ea98353e93dddfca94d28cf7cb2189
ashachakre0906 <chourpagar.asha@gmail.com>
2023-03-07 00:21:12 -0800

    wrote if else condition to compare secret number to user's guess

--- starter/script.js
+++ starter/script.js
@@ -1,16 +1,30 @@
 'use strict';
 // console.log(document.querySelector('.message'));
 //DOM is documnet object model basically structured representation of the html element allows access JS to access HTML elemnets and syles to manipulate them.
-document.querySelector('.message').textContent = "🎉 Correct Number";
-document.querySelector('.number').textContent = 13;
+// document.querySelector('.message').textContent = '🎉 Correct Number';
 document.querySelector('.score').textContent = 10;
 const buttonEl = document.querySelector('.check');
-buttonEl.addEventListener('click',function(){
-    console.log('button clicked');
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
+buttonEl.addEventListener('click', function () {
+  console.log('button clicked');
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
   // document.querySelector('.message').textContent = '🎉 Correct Number';
-   /*  console.log(guessValue,typeof guessValue); *///since guessValue is a string we need to convert this into a number so wrap it into the Number function.
+  /*  console.log(guessValue,typeof guessValue); */ //since guessValue is a string we need to convert this into a number so wrap it into the Number function.
   //Implemement the game logic
-
+  //Check if there is no input value
+  if (!guess) {
+    console.log('Please enter a guess value'); //0 is falsy value
+    document.querySelector('.message').textContent = 'Please enter the number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+  } else if (guess !== secretNumber) {
+    document.querySelector('.message').textContent = 'sorry, try again ☹️';
+  } else if (guess > secretNumber) {
+    console.log(guess > secretNumber);
+    console.log('hello');
+    document.querySelector('.message').textContent = 'hey';
+  }
 });
