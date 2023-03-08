a84cf245e80166151b0c8041f497274a05af7116
ashachakre0906 <chourpagar.asha@gmail.com>
2023-03-07 16:39:54 -0800

    changed the folder structure because the page was not getting deployed

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-'use strict';
+"use strict";
 // console.log(document.querySelector('.message'));
 //DOM is documnet object model basically structured representation of the html element allows access JS to access HTML elemnets and syles to manipulate them.
 // document.querySelector('.message').textContent = '🎉 Correct Number';
-document.querySelector('.score').textContent = 10;
-const buttonEl = document.querySelector('.check');
+document.querySelector(".score").textContent = 10;
+const buttonEl = document.querySelector(".check");
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
-buttonEl.addEventListener('click', function () {
-  console.log('button clicked');
-  const guess = Number(document.querySelector('.guess').value);
+buttonEl.addEventListener("click", function () {
+  console.log("button clicked");
+  const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
   // document.querySelector('.message').textContent = '🎉 Correct Number';
   /*  console.log(guessValue,typeof guessValue); */ //since guessValue is a string we need to convert this into a number so wrap it into the Number function.
   //Implemement the game logic
   //Check if there is no input value
-    //When there is no input
+  //When there is no input
   if (!guess) {
-    console.log('Please enter a guess value'); //0 is falsy value
-      document.querySelector('.message').textContent = 'Please enter the number';
+    console.log("Please enter a guess value"); //0 is falsy value
+    document.querySelector(".message").textContent = "Please enter the number";
     // When the player wins
   } else if (guess === secretNumber) {
-      document.querySelector('.message').textContent = '🎉 Correct Number';
-      document.querySelector('.number').textContent = secretNumber;
-      document.body.style.backgroundColor = 'green';
-      document.body.style.backgroundColor = '30rem';
+    document.querySelector(".message").textContent = "🎉 Correct Number";
+    document.querySelector(".number").textContent = secretNumber;
+    document.body.style.backgroundColor = "green";
+    document.body.style.backgroundColor = "30rem";
     //When guess is too high
   } else if (guess > secretNumber) {
     console.log(guess > secretNumber);
-      document.querySelector('.message').textContent = '📈Too high';
+    document.querySelector(".message").textContent = "📈Too high";
     // When guess is too low
   } else if (guess < secretNumber) {
-      document.querySelector('.message').textContent = '📉Too low';
+    document.querySelector(".message").textContent = "📉Too low";
   }
 });
