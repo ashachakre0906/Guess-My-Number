09beec0fedca6bdff5a4e8e56ac7ecc34f036d2d
ashachakre0906 <chourpagar.asha@gmail.com>
2023-03-07 22:18:47 -0800

    started refactoring the code using DRY principle

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const buttonEl = document.querySelector(".check");
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+}
 // document.querySelector(".highscore").textContent = highscore;
 
 // console.log(secretNumber);
